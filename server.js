f22374eae718613d4db40b33de2c014fc12ee692
tOOnPT <jose.miguel.malaca@me.com>
2014-12-18 13:14:43 +0000

    Added first files.
    Simples Hello Server working.

---
+++ server.js
@@ -0,0 +1,26 @@
+﻿var express = require('express'); //express module
+var request = require('request'); //request module
+var url = require('url'); //url module
+
+//get a express app started...
+var serverApp = express();
+
+//route definitio:... home
+serverApp.get('/', function (req, response) {
+    console.log("\n -Hello request");
+
+    request(url, function(err, res, body) {
+        response.render('hello.ejs');//file on views folder
+    });
+});
+
+//Start listen on port...
+var server = serverApp.listen(8080, function () {
+
+    var host = server.address().address;
+    var port = server.address().port;
+
+    if (host == "0.0.0.0")
+        host = "localhost";
+    console.log('\nApp listening at http://%s:%s', host, port);
+});
